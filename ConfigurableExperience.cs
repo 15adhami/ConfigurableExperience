6a245e862f5e70c8998ea6512d20717ae29d30da
15adhami <15adhami@gmail.com>
2021-10-19 00:07:17 -0700

    Add project files.

---
+++ ConfigurableExperience.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace ConfigurableExperience
+{
+	public class ConfigurableExperience : Mod
+	{
+		public ConfigurableExperience(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("com.github.15adhami.configurableexperience");
+			harmony.PatchAll();
+		}
+	}
+}
