5adf85d9f5c08bedfda4a88710b24d93807887eb
15adhami <15adhami@gmail.com>
2021-10-20 22:24:25 -0700

    - Fixed bugs

---
+++ Source/Patches/GetSkillDescription_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using XmlExtensions;
+
+namespace ConfigurableExperience.Patches
+{
+	[HarmonyPatch(typeof(SkillUI))]
+	[HarmonyPatch("GetSkillDescription")]
+	static class Patch_LearningSaturationUI
+	{
+		private static void Postfix(ref string __result, SkillRecord sk)
+		{
+			switch (sk.passion)
+			{
+				case Passion.None:
+					__result = Regex.Replace(__result, "[^:] 35%", new MatchEvaluator(delegate (Match m) { return m.Value[0] + " " + SettingsManager.GetSetting("imranfish.configurableexperience", "multiplierNoPassion") + "%"; }));
+					break;
+				case Passion.Minor:
+					__result = Regex.Replace(__result, "[^:] 100%", new MatchEvaluator(delegate (Match m) { return m.Value[0] + " " + SettingsManager.GetSetting("imranfish.configurableexperience", "multiplierMinorPassion") + "%"; }));
+					break;
+				case Passion.Major:
+					__result = Regex.Replace(__result, "[^:] 150%", new MatchEvaluator(delegate (Match m) { return m.Value[0] + " " + SettingsManager.GetSetting("imranfish.configurableexperience", "multiplierMajorPassion") + "%"; }));
+					break;
+			}
+		}
+	}
+}
--- Source/Patches/LearnRateFactor_Patch.cs
+++ Source/Patches/LearnRateFactor_Patch.cs
@@ -12,11 +12,8 @@ namespace ConfigurableExperience
 	{
 		static void Postfix(SkillRecord __instance, ref float __result, SkillDef ___def, bool direct = false)
 		{
-			// Global multiplier
-			__result *= float.Parse(SettingsManager.GetSetting("imranfish.configurableexperience", "globalMultiplier"));
-
 			// Stat multiplier
-			__result *= float.Parse(SettingsManager.GetSetting("imranfish.configurableexperience",  ___def.defName + "StatMultiplier"));
+			__result *= float.Parse(SettingsManager.GetSetting("imranfish.configurableexperience",  ___def.defName + "StatMultiplier")) / 100f;
 
 			switch (__instance.passion)
 			{
@@ -31,18 +28,6 @@ namespace ConfigurableExperience
 					break;
 				default:
 					throw new NotImplementedException("Passion level " + __instance.passion);
-			}
-
-			if (!direct)
-			{
-				if (__instance.LearningSaturatedToday)
-				{
-					__result /= 0.2f;
-				}
-				if (__instance.xpSinceMidnight > float.Parse(SettingsManager.GetSetting("imranfish.configurableexperience", "dailyThreshold")))
-				{
-					__result *= float.Parse(SettingsManager.GetSetting("imranfish.configurableexperience", "saturatedMultiplier"));
-				}
 			}
 		}
 	}
